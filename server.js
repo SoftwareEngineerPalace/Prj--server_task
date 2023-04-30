8473f98f373e0bace762ec7b3dd39975fa08aec7
xiaojianjun <jianjun.xiao@foxmail.com>
2023-04-30 20:21:00 +0800

    t

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://39.107.119.92");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // 创建 MySQL 数据库连接池
 const pool = mysql.createPool({
   host: "localhost",

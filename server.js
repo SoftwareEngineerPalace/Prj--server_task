59b72640e09c9fbedc2ab08a9d506f14e2742bf5
xiaojianjun <xiaojianjun@ele007.com>
2023-05-05 16:27:13 +0800

    e

--- server.js
+++ server.js
@@ -39,16 +39,24 @@ app.post("/saveTasks", async (req, res) => {
   const { id, name, priority, duration, deadline } = list[0]; // 只存入一条数据
 
   // 删除表中所有数据
-  pool.query(`DELETE FROM task`, (err, results) => {
-    if (err) throw err;
-    pool.query(
-      `INSERT INTO task (id, name, priority, duration, deadline) VALUES ('${id}', '${name}', ${priority}, ${duration}, '${deadline}')`,
-      (err, results) => {
-        if (err) throw err;
-        res.send("task added successfully");
-      }
-    );
-  });
+  // pool.query(`DELETE FROM task`, (err, results) => {
+  //   if (err) throw err;
+  //   pool.query(
+  //     `INSERT INTO task (id, name, priority, duration, deadline) VALUES ('${id}', '${name}', ${priority}, ${duration}, '${deadline}')`,
+  //     (err, results) => {
+  //       if (err) throw err;
+  //       res.send("task added successfully");
+  //     }
+  //   );
+  // });
+
+  pool.query(
+    `INSERT INTO task (id, name, priority, duration, deadline) VALUES ('${id}', '${name}', ${priority}, ${duration}, '${deadline}')`,
+    (err, results) => {
+      if (err) throw err;
+      res.send("task added successfully");
+    }
+  );
 });
 
 // 启动服务器
